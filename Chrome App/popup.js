8769237b3c840362500cee58f4835b504e69ceee
TauqirA <tauqir.abdullah@gmail.com>
2015-02-07 09:03:52 -0500

    Change Value to entered value

--- Chrome App/popup.js
+++ Chrome App/popup.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', function () {
 document.getElementById("submit").addEventListener("click", myFunction);
 });
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
+    document.getElementById("demo").innerHTML =  document.getElementById("text").value;;
 }
