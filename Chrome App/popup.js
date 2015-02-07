e3954b85460dc4d3f22464fa79efa24a31baf667
TauqirA <tauqir.abdullah@gmail.com>
2015-02-07 15:26:38 -0500

    Add Bing
    Google will not allow iframe so bing. Bing doesn''t respond to screen
    size but issue is not huge

--- Chrome App/popup.js
+++ Chrome App/popup.js
@@ -20,8 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function myFunction() {
-    var xmlhttp = new XMLHttpRequest();
     var word = document.getElementById("text").value.toLowerCase();
+	if(document.getElementById("Define").checked){
+    var xmlhttp = new XMLHttpRequest();
     var url = "http://api.wordnik.com/v4/word.json/" + word + "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -32,7 +33,32 @@ function myFunction() {
 
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+	}
+	else{
+	document.getElementById("definition").innerHTML = "";
+	}
 	var encode = encodeURIComponent(word);
-	document.getElementById("wolf").innerHTML = "<iframe id=\"if1\" width=\"100%\" height=\"100%\" style=\"visibility:visible\" FRAMEBORDER=\"0\" src=http://m.wolframalpha.com/input/?i="+encode+"></iframe>";
-	document.getElementById("wiki").innerHTML = "<iframe id=\"if1\" width=\"100%\" height=\"250\" style=\"visibility:visible\" FRAMEBORDER=\"0\" src=http://en.m.wikipedia.org/wiki/"+word+"></iframe>";
+	if(document.getElementById("wolfA").checked){
+		document.getElementById("wolf").innerHTML = "<iframe id=\"if1\" width=\"100%\" height=\"250\" style=\"visibility:visible\" FRAMEBORDER=\"0\" src=http://m.wolframalpha.com/input/?i="+encode+"></iframe>";
+	}
+	else{
+	document.getElementById("wolf").innerHTML = "";
+	}
+	if(document.getElementById("bingS").checked){
+		document.getElementById("bing").innerHTML = 
+		"<iframe id=\"if1\" width=\"100%\" height=\"250\" style=\"visibility:visible\" FRAMEBORDER=\"0\" src=http://m.bing.com/search?q="+encode+"></iframe>";
+	}
+	else{
+	document.getElementById("bing").innerHTML = "";
+	}
+	if(document.getElementById("wikip").checked){
+		document.getElementById("wiki").innerHTML = "<iframe id=\"if1\" width=\"100%\" height=\"250\" style=\"visibility:visible\" FRAMEBORDER=\"0\" src=http://en.m.wikipedia.org/wiki/"+word+"></iframe>";
+	}
+	else{
+	document.getElementById("wiki").innerHTML = "";
+	}
+	
+	/*chrome.tabs.executeScript(null,
+      {code:"document.body.style.backgroundColor='red'"});
+	    window.close();*/
 }
